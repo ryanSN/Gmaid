2efb288e67b84315a6ef7a43161d891c7c7f0122
Ryan Chatterton <ryan.chatterton@sharpnotions.com>
2018-04-30 07:36:08 -0400

    Update and add in Social and Promotions section.

--- gmaid.js
+++ gmaid.js
@@ -1,11 +1,27 @@
 //Alex Maki-Jokela
 //Written sometime in 2016ish
+//Updated by Ryan Chatterton 2018ish
 
-function cleanUp() {
-  var delayDays = 2 // Enter # of days before messages are moved to archive
+function cleanInbox() {
+  var delayDays = 3 // Enter # of days before messages are moved to archive
+  
   var maxDate = new Date();
   maxDate.setDate(maxDate.getDate()-delayDays);
-  var threads = GmailApp.getInboxThreads();
+  
+  var threads = GmailApp.getInboxThreads(0,500);
+  
+  for (var i = 0; i < threads.length; i++) {
+    if (threads[i].getLastMessageDate()<maxDate && !threads[i].hasStarredMessages())
+      {
+        threads[i].moveToArchive();
+      }
+  }
+}
+
+function cleanPromotionsAndSocial() {
+  var delayDays = 3;
+  var maxDate = new Date();
+  var threads = GmailApp.search('in:promos OR in:social');
   for (var i = 0; i < threads.length; i++) {
     if (threads[i].getLastMessageDate()<maxDate && !threads[i].hasStarredMessages())
       {
